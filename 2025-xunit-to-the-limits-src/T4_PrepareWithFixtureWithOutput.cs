140de15bf6cbe1942d64ddd422edc278e21b85ad
guillaume prof <guillaume.saintetienne@iter.org>
2025-03-16 18:59:19 +0100

    test class that needs logger

---
+++ 2025-xunit-to-the-limits-src/T4_PrepareWithFixtureWithOutput.cs
@@ -0,0 +1,49 @@
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
+
+
+namespace _2025_xunit_to_the_limits_src;
+
+public class T3_PrepareWithFixtureWithOutput  : IClassFixture<SimpleSyncLifeTime>
+{
+    private readonly SimpleSyncLifeTime _fixture;
+
+
+    [Fact]
+    public void Test1()
+    {
+       
+        _fixture.TestableValue.Should().Be(42);
+    }
+    
+    [Fact]
+    public void TestClassExpectingLoggerButWhere()
+    {
+        var sutClass = new T4_SutClass(null);
+        sutClass.TestableValue.Should().Be(88);
+    }
+    
+    // need to have that fixture
+    public T3_PrepareWithFixtureWithOutput(SimpleSyncLifeTime fixture, ITestOutputHelper outputHelper)
+    {
+        _fixture = fixture;
+        outputHelper.WriteLine(fixture.TestableValue.ToString());
+        
+        
+       // outputHelper is not ILogger 🤔
+        
+        
+       // this.TestLogger = outputHelper.ToLogger<SimpleSyncLifeTime>();
+    }
+
+    public ILogger  TestLogger { get; init; }
+    
+    
+    [Fact]
+    public void TestClassExpectingLogger()
+    {
+        var sutClass = new T4_SutClass(this.TestLogger);
+        sutClass.TestableValue.Should().Be(88);
+    }
+}
---
+++ 2025-xunit-to-the-limits-src/T4_SutClass.cs
@@ -0,0 +1,13 @@
+using Microsoft.Extensions.Logging;
+
+namespace _2025_xunit_to_the_limits_src;
+
+public class T4_SutClass
+{
+    public T4_SutClass(ILogger  testLogger)
+    {
+        testLogger.LogInformation("logger available in SutClass");
+    }
+
+    public object TestableValue { get; } = 88;
+}
